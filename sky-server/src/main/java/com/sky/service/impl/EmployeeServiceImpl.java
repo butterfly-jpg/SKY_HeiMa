2941737cfe1e6b3acbbb501263b98b420cade3e4
czk <czk@qq.com>
2024-07-23 22:06:05 +0800

    7-23 version 2.1 完成新增员工功能 解决了两个问题： 1、录入的用户名已存在抛出异常没有处理（使用全局异常类对该异常进行管理） 2、新增员工时，创建人Id和修改人Id设置为固定值（通过ThreadLocal线程对JWT令牌token进行解析得到id）

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -86,8 +87,9 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setUpdateTime(LocalDateTime.now());
 
         //设置当前记录创建人Id和修改人Id
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+        //从ThreadLocal中得到id
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
     }
